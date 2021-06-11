7cba5089c078321286ca8d185b007d2f62f891ae
Jan Marek <jan.marek.cz@gmail.com>
2021-06-11 14:26:20 +0200

    [WIP] Added db endpoints

--- app/service.py
+++ app/service.py
@@ -3,7 +3,7 @@ from flask import jsonify
 from datetime import datetime
 
 from app.config import FEED_URL
-from app.model import Article, db
+from app.model import Article, db, Questions, Answers
 
 
 def get_mock_text():
@@ -42,9 +42,9 @@ def get_articles_from_feed():
         d['title'] = i['title']
         d['image_url'] = i['szn_image']
         d['buttons'] = [{"type": "show_block",
-                        "title": "TO MĚ ZAJIMÁ",
-                        "block_names": ["Article"],
-                        "set_attributes": {"ArticleID": i['id']}}]
+                         "title": "TO MĚ ZAJIMÁ",
+                         "block_names": ["Article"],
+                         "set_attributes": {"ArticleID": i['id']}}]
         l.append(d)
     response = jsonify({
         "messages": [
@@ -107,3 +107,80 @@ def update_articles_in_db():
             counter = counter + 1
     db.session.commit()
     return counter
+
+
+def get_articles_from_db():
+    articles = Article.query \
+        .order_by(Article.published_date.desc()) \
+        .limit(5) \
+        .all()
+
+    results = [article.article_article_dto_converter() for article in articles]
+
+    return jsonify({
+        "messages": [
+            {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "image_aspect_ratio": "square",
+                        "elements": results
+                    }
+                }
+            }
+        ]
+    })
+
+
+def get_article_from_db(pk_id):
+    article = Article.query.get(pk_id)
+
+    return jsonify({"messages": article.article_article_detail_dto_converter()})
+
+
+def get_question_from_db(questionID):
+    question = Questions.query.get(questionID)
+    answears = Answers.query.filter(Answers.question_id == question.id).all()
+
+    buttons = []
+    for i in answears:
+        d = {
+            "block_names": [
+                "ShowTextTest"
+            ],
+            "set_attributes": {
+                "ArticleID": question.news_id,
+                "QuizID": question.id,
+                "AnswerID": i.id
+            },
+            "title": i.answer_text,
+            "type": "show_block"
+        }
+        buttons.append(d)
+
+    t = {
+        "buttons": buttons,
+        "title": question.question_text
+    }
+    return jsonify({
+        "messages": [
+            {
+                "attachment": {
+                    "payload": {
+                        "elements": [t],
+                        "template_type": "generic"
+                    },
+                    "type": "template"
+                }
+            }
+        ]
+        })
+
+
+def verify_answer(answerID):
+    answer = Answers.query.get(answerID)
+
+    result = 'spravna' if answer.correct_answers else 'spatna'
+
+    return jsonify({"Vysledek": "Vase odpoved byla {}".format(result)})

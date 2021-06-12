ce48d7f35e5ea90e0fbc5818f99c802228761afd
petrochvat <petrochvat@gmail.com>
2021-06-12 22:10:08 +0200

    comment in main for local development SQLALCHEMY_DATABASE_URI

--- app/main.py
+++ app/main.py
@@ -6,9 +6,9 @@ from app.controller import api
 
 app = Flask(__name__)
 app.register_blueprint(api)
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://newschatbotdevelopment:Wlk8skrHKvZEbM6Gw@database.internal.newschatbot.ceskodigital.net:5432/newschatbotdevelopment"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://newschatbotdevelopment:Wlk8skrHKvZEbM6Gw@database.internal.newschatbot.ceskodigital.net:5432/newschatbotdevelopment"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/feed_parser"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/feed_parser"
 migrate = Migrate(app, db)
 db.init_app(app)
 

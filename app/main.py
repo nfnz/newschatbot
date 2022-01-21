6dbbcbd5dea1117f02deab8cab7ccdab0de00f8a
Michal Vlasák <daeatel@gmail.com>
2022-01-21 14:09:10 +0100

    Move DB_CONNSTR to serverless.yml

--- app/main.py
+++ app/main.py
@@ -8,10 +8,7 @@ from app.controller import api
 
 app = Flask(__name__)
 app.register_blueprint(api)
-db_connstr = os.environ.get(
-    "DB_CONNSTR",
-    "postgresql://newschatbotdevelopment:Wlk8skrHKvZEbM6Gw@database.internal.newschatbot.ceskodigital.net:5432/newschatbotdevelopment",
-)
+db_connstr = os.environ.get("DB_CONNSTR")
 app.config["SQLALCHEMY_DATABASE_URI"] = db_connstr
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 migrate = Migrate(app, db)
